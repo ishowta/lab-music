7322810a921c45078dd478e0ba6bbc0300f6ebe8
kinironote <kinironote@gmail.com>
2017-10-13 12:10:56 +0900

    [update] 複数の曲をストックできるようにした

--- medialabsound.py
+++ medialabsound.py
@@ -2,60 +2,87 @@ import urllib.parse
 import requests
 import json
 import os
-import pygame
-from playsound import playsound
-import shutil
-#from mplayer import Player
 from subprocess import Popen, PIPE
+import time
+import pprint
+import os
+os.mkdir("music")
 
-channelID = "C7H8KHS69"
-token = open('token','r').read()
+#channelID = "C7H8KHS69"
+channelID = "C7HQXCU21"
+token = open('token','r').read().rstrip('\n').rstrip('\r\n')
 
-#pygame.mixer.init()
+mplayer = []
 
-pre_title = ""
+def playMusic(music_data):
+	[title, link] = music_data
+	path = "music/" + title
+	global mplayer
+	r = requests.get(link, headers={'Authorization': 'Bearer %s' % token})
+	if r.status_code != 200:
+		raise Exception("Cannot connected Slack server!")
+	with open(path, 'wb') as f:
+		for chunk in r:
+			f.write(chunk)
+	pipes = dict(stdin=PIPE, stdout=PIPE, stderr=PIPE)
+	mplayer = Popen(["mplayer", path], **pipes)
 
-def getData():
-	global pre_title
-	par = {
+def getSlackData():
+	par = urllib.parse.urlencode({
 		'token':token,
 		'channel':channelID,
 		'count':1
-	}
-
-	par = urllib.parse.urlencode(par)
+	})
 	res = requests.get("https://slack.com/api/channels.history",params=par)
-	#req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-	#req.add_data(params)
-	#res = urllib3.urlopen(req)
-	#print(res.text)
 	data = json.loads(res.text)
-	link = data["messages"][0]["file"]["url_private_download"]
+	return data
+
+def isMusicData(data):
+	if "subtype" in data["messages"][0] and data["messages"][0]["subtype"] == "file_share":
+		return True
+	else:
+		return False
+
+def getMusicData(data):
 	title = data["messages"][0]["file"]["name"]
-	if title != pre_title:
-		pre_title = title
-		print(link)
-		print(title)
-		r = requests.get(link, headers={'Authorization': 'Bearer %s' % token})
-		if r.status_code == 200:
-			print('200 OK')
-			with open(title, 'wb') as f:
-				#r.raw.decode_content = true
-				#shutil.copyfileobj(r.raw, f)
-				for chunk in r:
-					f.write(chunk)
-		pipes = dict(stdin=PIPE, stdout=PIPE, stderr=PIPE)
-		mplayer = Popen(["mplayer", title], **pipes)
-		mplayer.communicate(input=b">")
-		#sys.stdout.flush()
-		#player = Player()
-		#player.loadfile(title)
-		#os.system("wget -O "+title+" "+link)
-		#playsound(title)
-		#pygame.mixer.music.load(title)
-		#pygame.mixer.music.play()
-		#while pygame.mixer.music.get_busy() == True:
-		#	continue
-		print("finish run")
+	link = data["messages"][0]["file"]["url_private_download"]
+	return [title, link]
+
+pre_data = []
+def checkUpdate(data):
+	global pre_data
+	if pre_data == data:
+		return False
+	pre_data = data
+	return True
+
+def isMusicStop():
+	if not mplayer or (mplayer and mplayer.poll() == 0):
+		return True
+	else:
+		print("false")
+		return False
+
+music_data_list = []
+
 while True:
-	getData()
+	print("loop")
+	# Check Slack
+	data = getSlackData()
+	if checkUpdate(data):
+		print("a")
+		if isMusicData(data):
+			print("aa")
+			music_data_list.append(getMusicData(data))
+			print("Now stashed")
+			for cnt, music_data in enumerate(music_data_list):
+				print(str(cnt+1)+": "+music_data[0].split('.')[0])
+
+	# Play music
+	if isMusicStop() and music_data_list:
+		music_data = music_data_list.pop(0)
+		print("Play: "+music_data[0].split('.')[0])
+		playMusic(music_data)
+
+	# Wait 1 sec
+	time.sleep(1)
