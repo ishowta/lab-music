fa9f0a71eb21ff6e9f45f0e858522aad8b0368b5
kinironote <kinironote@gmail.com>
2017-10-18 04:12:04 +0000

    Ironna Command ni Taio

--- medialabsound.py
+++ medialabsound.py
@@ -6,10 +6,11 @@ from subprocess import Popen, PIPE
 import time
 import pprint
 import os
-os.mkdir("music")
+if not os.path.exists("music"):
+	os.mkdir("music")
 
-#channelID = "C7H8KHS69"
-channelID = "C7HQXCU21"
+channelID = "C7H8KHS69"
+#test channelID = "C7HQXCU21"
 token = open('token','r').read().rstrip('\n').rstrip('\r\n')
 
 mplayer = []
@@ -37,6 +38,14 @@ def getSlackData():
 	data = json.loads(res.text)
 	return data
 
+def sendMessage(text):
+        par = urllib.parse.urlencode({
+                'token':token,
+                'channel':channelID,
+                'text':text
+        })
+        res = requests.get("https://slack.com/api/chat.postMessage",params=par)
+
 def isMusicData(data):
 	if "subtype" in data["messages"][0] and data["messages"][0]["subtype"] == "file_share":
 		return True
@@ -60,28 +69,51 @@ def isMusicStop():
 	if not mplayer or (mplayer and mplayer.poll() == 0):
 		return True
 	else:
-		print("false")
 		return False
 
 music_data_list = []
+
+onryo = 100
+
+mplayer_terminated = False
 
 while True:
 	print("loop")
 	# Check Slack
 	data = getSlackData()
 	if checkUpdate(data):
-		print("a")
 		if isMusicData(data):
-			print("aa")
 			music_data_list.append(getMusicData(data))
-			print("Now stashed")
+			sendMessage("再生リスト")
 			for cnt, music_data in enumerate(music_data_list):
-				print(str(cnt+1)+": "+music_data[0].split('.')[0])
+				sendMessage(str(cnt+1)+": "+music_data[0].split('.')[0])
+		text = data["messages"][0]["text"]
+		if text == 'up' and onryo != 100:
+			onryo = onryo + 5
+			os.system('amixer cset numid=1 '+str(onryo)+'%')
+			sendMessage("Set "+str(onryo)+"%")
+		if text == 'down' and onryo != 85:
+			onryo = onryo - 5
+			os.system('amixer cset numid=1 '+str(onryo)+'%')
+			sendMessage("Set "+str(onryo) + "%")
+		if mplayer and text == 'skip':
+			mplayer.terminate()
+			mplayer_terminated = True
+		if mplayer and text == 'stop':
+			music_data_list = []
+			mplayer.terminate()
+			mplayer_terminated = True
+		if mplayer and text.split(' ')[0] == 'urusai':
+			mplayer.terminate()
+			print(float(text.split(' ')[1]) * 60)
+			time.sleep(float(text.split(' ')[1]) * 60)
+			mplayer_terminated = True
 
 	# Play music
-	if isMusicStop() and music_data_list:
+	if (isMusicStop() or mplayer_terminated) and music_data_list:
+		mplayer_terminated = False
 		music_data = music_data_list.pop(0)
-		print("Play: "+music_data[0].split('.')[0])
+		sendMessage("Now playing: "+music_data[0].split('.')[0])
 		playMusic(music_data)
 
 	# Wait 1 sec
